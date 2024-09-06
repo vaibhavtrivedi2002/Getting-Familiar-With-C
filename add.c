3113e072a51481ce06be06c540ac590bb2859308
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-09-06 22:46:49 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,12 @@
+//this program add two numbers
+#include <stdio.h>
+int main() {
+    int a,b,sum;
+    printf("Enter number a:");
+    scanf("%d",&a);
+    printf("Enter number b:");
+    scanf("%d",&b);
+    // sum=a+b;
+    printf("Sum is:%d",a+b);
+    return 0;
+}
