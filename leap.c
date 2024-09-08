a42276ffcb3894090faf9d22989302a13fe5cbd6
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-09-08 20:24:58 +0530

    Add files via upload

---
+++ leap.c
@@ -0,0 +1,15 @@
+//this program checks whether the given year is leap or not
+#include<stdio.h>
+int main(){
+    int year,isLeapYear;
+    printf("Enter year:");
+    scanf("%d",&year);
+    isLeapYear=(year % 4 == 0) ? ((year % 100 != 0) ? 1 : (year % 400 == 0 ? 1 : 0)) : 0;
+      if (isLeapYear) {
+        printf("%d is a leap year.\n", year);
+    } else {
+        printf("%d is not a leap year.\n", year);
+    }
+
+    return 0;
+}
