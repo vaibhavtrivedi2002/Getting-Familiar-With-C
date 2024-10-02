e966d94437d7cd6c3f467193cbd34d8fc3d1d4a3
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-10-02 16:36:56 +0530

    Delete evodd.c

--- evodd.c
+++
@@ -1,9 +0,0 @@
-//this program finds the given number is odd or even
-#include <stdio.h>
-int main() {
-    int n;
-    printf("Enter a number:");
-    scanf("%d",&n);
-    n%2==0?printf("%d is even",n):printf("%d is odd",n);
-    return 0;
-}
